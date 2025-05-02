6120869d03aabad538eeee46925a3343509dbbac
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-02 01:08:06 -0300

    chore: developed the navBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { HeroSection } from "./components/HeroSection";
-
+import { NavBar } from "./components/NavBar";
 export function App() {
   return (
-    <>
+    <main className="antialiased overflow-hidden text-neutral-200">
       <HeroSection />
-    </>
+      <NavBar />
+    </main>
   );
 }
